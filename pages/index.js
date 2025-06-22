083a07d8b0159a07554518218b484ac6f398f4c6
alcimarcostabrasil <115326256+alcimarcostabrasil@users.noreply.github.com>
2025-06-22 22:11:00 +0000

    renomenando conteúdo principal

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,7 @@ function Home(){
     return(
         <div>
             <h1>Bem vindo ao meu primeiro programa next</h1>
-            <h2>Ofereço esse treinamento a minha linda esposa Zeneide e 
-                aos meus filhos maravilhosos Mirella e Apollo. Amo vocês. Se você me ama dê uma risadinha!</h2>
+            <h2>Projeto do curso de Nestjs</h2>
         </div>
     )
 }
