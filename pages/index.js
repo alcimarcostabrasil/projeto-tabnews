b6e304e1c1eb9b18d5212d7d9fd92dc3ec5a8a5e
alcimarcostabrasil <115326256+alcimarcostabrasil@users.noreply.github.com>
2025-06-22 21:31:57 +0000

    inserindo piadinha na frase

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home(){
         <div>
             <h1>Bem vindo ao meu primeiro programa next</h1>
             <h2>Ofereço esse treinamento a minha linda esposa Zeneide e 
-                aos meus filhos maravilhosos Mirella e Apollo. Amo vocês. </h2>
+                aos meus filhos maravilhosos Mirella e Apollo. Amo vocês. Se você me ama dê uma risadinha!</h2>
         </div>
     )
 }
