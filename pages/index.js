2fa455a0f31dda5e20aad44eca103fb6b730c308
alcimarcostabrasil <115326256+alcimarcostabrasil@users.noreply.github.com>
2025-06-22 22:12:30 +0000

    ajustando a grafia do framework

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home(){
     return(
         <div>
             <h1>Bem vindo ao meu primeiro programa next</h1>
-            <h2>Projeto do curso de Nestjs</h2>
+            <h2>Projeto do curso de Nextjs</h2>
         </div>
     )
 }
