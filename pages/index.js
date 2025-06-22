12cbbe807c316287ad1b34d56621bae27ec8a028
alcimarcostabrasil <115326256+alcimarcostabrasil@users.noreply.github.com>
2025-06-22 20:36:46 +0000

    adicionando pagina principal

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+    return(
+        <div>
+            <h1>Bem vindo ao meu primeiro programa next</h1>
+            <h2>Ofereço esse treinamento a minha linda esposa Zeneide e 
+                aos meus filhos maravilhosos Mirella e Apollo. Amo vocês. </h2>
+        </div>
+    )
+}
+export default Home;
